7da09bda4cfd1af4566494655082cd62b2c630c2
Kaio xavier <152352968+oasiskaio@users.noreply.github.com>
2024-03-20 00:16:05 -0300

    evidencia

--- Orientação a objetos/Agregração/04_2.py
+++
@@ -1,9 +0,0 @@
-from contas import Conta
-from clientes import Cliente
-client1 = Cliente(123, "Joao", "Rua 1")
-client2 = Cliente(345, "Maria", "Rua 2")
-conta1 = conta([cliente1,client2], 1, 0)
-conta1.gerarsaldo()
-conta1.depositar(1500)
-conta1.sacar(500)
-conta1.gerarsaldo()
--- Orientação a objetos/Herança/01.py
+++ Orientação a objetos/Herança/01.py
@@ -9,15 +9,15 @@ class ClasseSomaMultiplica:
         return self.a * self.b;
     
 
-    class Derivada(ClasseSomaMultiplica):
-        def subtrair(self):
+class Derivada(ClasseSomaMultiplica):
+    def subtrair(self):
          return self.a - self.b;
-        def multiplicar(self):
+    def multiplicar(self):
             return self.a * self.b;
         
-    d = Derivada(20, 10)
-    print('A soma de 20 + 10 é igual a {d.somar()}') #vai mostrar a soma
-    print(issubclass(Derivada, ClasseSomaMultiplica)) #Faz o teste se a Derivada é item herança de classeSomaMultiplica
+d = Derivada(20, 10)
+print('A soma de 20 + 10 é igual a {d.somar()}') #vai mostrar a soma
+print(issubclass(Derivada, ClasseSomaMultiplica)) #Faz o teste se a Derivada é item herança de classeSomaMultiplica
 
     #vai retornar  "A somo de 20 + 10 é igual a 30"
     #E a segunda linhga vai ser igual a True
